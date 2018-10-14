532e884b14edd58cf656b1208dc140ab5024f85e
meeshen <mishival@gmail.com>
2018-10-14 09:54:13 +0200

    small fixes

--- lumber_example/src/lumbermill_node.cpp
+++ lumber_example/src/lumbermill_node.cpp
@@ -10,22 +10,20 @@
 ros::Publisher planks_pub;
 ros::Subscriber lumber_sub;
 
-std_msgs::Int64 lumber_msg;
 std_msgs::Int64 planks_msg;
 
 // pubs and subs don't need to be init'd in the main loop, just check that the object scopes are correct
 void node_init()
 {
-  ros::NodeHandle n;
+  ros::NodeHandle n; // can be risky to limit handle's scope to the init function only
   planks_pub = n.advertise<std_msgs::Int64>("planks_to_shop", 10);
   
   // has to subscribe to an existing topic (ours is created by lumberjack node)
   // calls back to lumber_callback when it receives a message
-  angular_velocity_sub = n.subscribe("lumber_to_mill", 10, lumber_callback);
+  lumber_sub = n.subscribe("lumber_to_mill", 10, lumber_callback);
 }
 
-// callback for lumber_sub, called whenever the sub receives a new message
-void lumber_callback(const std_msgs::Int64::ConstPtr& lumber_msg)
+void lumber_callback(const std_msgs::Int64::ConstPtr &lumber_msg)
 {
   // unpack the message
   int logs = lumber_msg->data;
@@ -33,7 +31,8 @@ void lumber_callback(const std_msgs::Int64::ConstPtr& lumber_msg)
   planks_msg.data = log_cutter(logs);
   
   // publish the planks
-  planks_pub.publish(plank_msg);
+  planks_pub.publish(planks_msg);
+  ROS_DEBUG("sent %d planks to the shop",planks_msg.data);
 }
 
 // function to cut logs
@@ -50,6 +49,7 @@ int log_cutter(int logs)
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "lumbermill_node");
+  node_init();
   ros::Rate loop_rate(10); // "never sleeps" so 10 Hz :^)
 
   // below is pretty much equal to ros::spin()
