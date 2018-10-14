8e8b9c40b991e61160431b9f8fc80179fa5823e8
meeshen <mishival@gmail.com>
2018-10-14 10:02:29 +0200

    Create lumbershop_node.cpp

---
+++ lumber_example/src/lumbershop_node.cpp
@@ -0,0 +1,45 @@
+/*
+  * Lumbershop node. Takes planks and sells them or whatever.
+*/
+
+#include "ros/ros.h"
+#include "std_msgs/Int64.h"
+
+// subscriber
+ros::Subscriber planks_sub;
+
+void node_init()
+{
+  ros::NodeHandle n;
+  planks_sub = n.subscribe("planks_to_shop", 10, shop_callback);
+}
+
+void shop_callback(const std_msgs::Int64::ConstPtr &planks_msg)
+{
+  int planks = planks_msg->data;
+  ROS_DEBUG("received %d planks",planks);
+
+  // now do something with them, like sell them or whatever
+  planks = process_planks(planks);
+}
+
+// processing planks
+int process_planks(int planks)
+{
+  // haha I'm just trashing them
+  int planks_before = planks;
+  planks = 0;
+  ROS_DEBUG("threw away %d planks, fuck nature and profit", planks_before);
+  return planks;
+}
+
+// very clean main loop
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "lumbershop_node");
+  node_init();
+  ros::Rate loop_rate(5);
+  ros::spin();
+
+  return 0;
+}
