83ffc0064575fa3f011347056e81d87071b4d701
meesh <mishival@gmail.com>
2018-11-15 20:16:31 +0100

    minor bugfixes, comments

--- testrig/scripts/keyboard_pin_node.py
+++ testrig/scripts/keyboard_pin_node.py
@@ -24,40 +24,57 @@ class KeyboardPinNode:
     def run(self):
         print("Set motor pins directly.")
         print("Format: EN1 DIR1 EN2 DIR2 EN3 DIR3 EN4 DIR4")
+
         command = ''
         while 1:
+            # wait for input
             command = raw_input('# ')
+
             if command == 'quit' or command == 'exit' or command == 'q':
                 print("Exiting. Thank you for your service.")
                 break
-            input_error = 0
+
+            # remove spaces
             command = command.replace(' ','')
-
             com_len = len(command)
-            if com_len!=8:
-                input_error = 1
+            digs = [0]*com_len
 
-            digs = [0]*8
+            # error accumulation
+            input_error = 0
+
+            # wrong length
+            if com_len!=8:
+                input_error += 1
+
             for i in range(com_len):
                 if command[i]!='0' and command[i]!='1':
-                    input_error = 2
+                    input_error += 2
                     break
                 else:
                     digs[i] = int(command[i])
+
+            # no errors
             if input_error==0:
-                # if no errors, unpack. Format: enables 1-4, dirs 1-4
+                # reformat pin order to the one used by ino
                 self.pins = digs[0]*128 + digs[2]*64 + digs[4]*32 + digs[6]*16 + digs[1]*8 + digs[3]*4 + digs[5]*2 + digs[7]
-                print('Success. Sending value: {0}\n'.format(self.pins))
+                # add 512 -- extra timeout pin
+                self.pin += 512
+
+                print('Success. Sending value: {0} (= {1} without extra time flag)\n'.format(self.pins, self.pins-512))
                 self.publish()
-            elif input_error==1:
+            # error type 1
+            if input_error&1:
                 print('Wrong argument length.\n')
-            elif input_error==2:
+            # error type 2
+            if input_error&2:
                 print('Only 1s and 0s allowed.\n')
-            else:
-                print("Unknown error.\n")
+
     def publish(self):
+        # pin_msg is of type Int64
         pins_msg = Int64()
+        # pack pins to pin_msg
         pins_msg.data = self.pins
+        # pub
         self.pub.publish(pins_msg)
 
 
