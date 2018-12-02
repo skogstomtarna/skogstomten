307d0a4abb744f27bfe836ad57bf48b249cf335e
tgeiger12 <tgeiger@kth.se>
2018-12-02 19:19:32 +0100

    updated the second arduino-code

--- Sonic/ultrasonic2.cpp/ultrasonic2.cpp.ino
+++ Sonic/ultrasonic2.cpp/ultrasonic2.cpp.ino
@@ -23,121 +23,100 @@ const int ledPin4 = 4;
 // defines variables
 long duration;
 int distance;
+const int warning = 100;  // Sets the distance of which the LED's react to.
 
 void setup() {
   nh.getHardware()->setBaud(57600);
   nh.initNode();
-  nh.advertise(sonic5);
+  nh.advertise(sonic5); // Advertises the topics to ROS
   nh.advertise(sonic6);
   nh.advertise(sonic7);
   nh.advertise(sonic8);
  
-  
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin5, INPUT); // Sets the echoPin as an Input
   pinMode(echoPin6, INPUT); // Sets the echoPin as an Input
   pinMode(echoPin7, INPUT); // Sets the echoPin as an Input
   pinMode(echoPin8, INPUT); // Sets the echoPin as an Input
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
-  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin1, OUTPUT); // Sets the ledPin1 as an Output
+  pinMode(ledPin2, OUTPUT); // Sets the ledPin2 as an Output
+  pinMode(ledPin3, OUTPUT); // Sets the ledPin3 as an Output
+  pinMode(ledPin4, OUTPUT); // Sets the ledPin4 as an Output
   Serial.begin(57600); // Starts the serial communication
 }
 
-void loop() {
-  
-  // Reads the echoPin, returns the sound wave travel time in microseconds
+// Function to activate the trigger pulse
+void trigger(){
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+}
 
-  for(int i=4;i<8;i++){
-    // Clears the trigPin
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    switch(i){
-      case 4:
-        duration = pulseIn(echoPin5, HIGH);
-        distance = duration/29/2;
-        Distance5.data=distance;
-<<<<<<< HEAD
-        if(distance<50){
-=======
-        if(distance<100){
->>>>>>> 8cd8a82ff50b949f939efcd8b7c87625fb2a3eba
-          digitalWrite(ledPin1, HIGH);
-        }
-        else{
-          digitalWrite(ledPin1, LOW); 
-        }
-        //delay(10);
-      break;
-      case 5:
-        duration = pulseIn(echoPin6, HIGH);
-        distance = duration/29/2;
-        Distance6.data=distance;
-<<<<<<< HEAD
-        if(distance<50){
-=======
-        if(distance<100){
->>>>>>> 8cd8a82ff50b949f939efcd8b7c87625fb2a3eba
-          digitalWrite(ledPin2, HIGH);
-        }
-        else{
-          digitalWrite(ledPin2, LOW); 
-        }
-        //delay(10);
-      break;
-      case 6:
-        duration = pulseIn(echoPin7, HIGH);
-        distance = duration/29/2;
-        Distance7.data=distance;
-<<<<<<< HEAD
-        if(distance<50){
-=======
-        if(distance<100){
->>>>>>> 8cd8a82ff50b949f939efcd8b7c87625fb2a3eba
-          digitalWrite(ledPin3, HIGH);
-        }
-        else{
-          digitalWrite(ledPin3, LOW); 
-        }
-        //delay(10);
-      break;
-      case 7:
-        duration = pulseIn(echoPin8, HIGH);
-        distance = duration/29/2;
-        Distance8.data=distance;
-<<<<<<< HEAD
-        if(distance<50){
-=======
-        if(distance<100){
->>>>>>> 8cd8a82ff50b949f939efcd8b7c87625fb2a3eba
-          digitalWrite(ledPin4, HIGH);
-        }
-        else{
-          digitalWrite(ledPin4, LOW); 
-        }
-        //delay(10);
-        break;
-    }
-    //delay(10);
+void loop() {
+  // pulseIn is a blocking function, thus we need to run each sensor one at a time.
+  
+  // First sensor
+  trigger();
+  duration = pulseIn(echoPin5, HIGH, 100000);
+  distance = duration/29/2;               // "/29/2" speed of sound travelling back and forth
+  Distance5.data=distance;
+  if(distance<warning && distance != 0){  // Distance results in "0" if no return signal is recieved.
+    digitalWrite(ledPin1, HIGH);
   }
+  else{
+    digitalWrite(ledPin1, LOW); 
+  }
+  delay(10);
+
+  //Second sensor
+  trigger();
+  duration = pulseIn(echoPin6, HIGH, 100000);
+  distance = duration/29/2;
+  Distance6.data=distance;
+  if(distance<warning && distance != 0){
+    digitalWrite(ledPin2, HIGH);
+  }
+  else{
+    digitalWrite(ledPin2, LOW); 
+  }
+  delay(10);
+
+  // Third sensor
+  trigger();
+  duration = pulseIn(echoPin7, HIGH, 100000);
+  distance = duration/29/2;
+  Distance7.data=distance;
+  if(distance<warning && distance != 0){
+    digitalWrite(ledPin3, HIGH);
+  }
+  else{
+    digitalWrite(ledPin3, LOW); 
+  }
+  delay(10);
+
+  // Fourth sensor
+  trigger();
+  duration = pulseIn(echoPin8, HIGH, 100000);
+  distance = duration/29/2;
+  Distance8.data=distance;
+  if(distance<warning && distance != 0){
+    digitalWrite(ledPin4, HIGH);
+  }
+  else{
+    digitalWrite(ledPin4, LOW); 
+  }
+  delay(10);
   
   //Publishing data
   sonic5.publish(&Distance5);
-  //delay(10);
   sonic6.publish(&Distance6);
-  //delay(10);
   sonic7.publish(&Distance7);
-  //delay(10);
   sonic8.publish(&Distance8);
-  //delay(10);
   
   nh.spinOnce();
-  
   delay(100);
 }
